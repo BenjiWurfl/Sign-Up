6c8a8e22ebb2bec219f0d5c44ee18286e0cdf4b1
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2023-11-13 11:15:42 +0100

    SignUp formular

--- signUp.js
+++ signUp.js
@@ -1,6 +1,6 @@
 function redirectLogin() {
     window.location.href = "https://benjiwurfl.github.io/Login";
 }
-function redirectApp() {
-    window.location.href = "https://thinkwisenotes.webflow.io/app";
-}
+//function redirectApp() {
+//    window.location.href = "https://thinkwisenotes.webflow.io/app";
+//}
