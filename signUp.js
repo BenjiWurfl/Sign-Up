fd9b7e78a81305a3593e71a6d2a59e0738accb67
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2023-11-16 07:56:49 +0100

    Noch nicht fertig

--- signUp.js
+++ signUp.js
@@ -1,6 +1,55 @@
 function redirectLogin() {
     window.location.href = "https://benjiwurfl.github.io/Login/";
 }
-//function redirectApp() {
-//    window.location.href = "https://thinkwisenotes.webflow.io/app";
-//}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
+    authDomain: "thinkwise-c7673.firebaseapp.com",
+    databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "thinkwise-c7673",
+    storageBucket: "thinkwise-c7673.appspot.com",
+    messagingSenderId: "37732571551",
+    appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
+    measurementId: "G-8957WM4SB7"
+};
+
+// Initialize Firebase
+const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+
+signupbutton.addEventListener('click', (e) => {
+
+    var email = document.getElementById('inputEmail').value;
+    var password = document.getElementById('inputPassword').value;
+    var name = document.getElementById('inputName').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert('user created');
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            alert(errorMessage);
+            // ..
+        });
+
+
+})
