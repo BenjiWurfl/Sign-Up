49a4e8ae250dedd6b6cb03d97b662bc79fa13b4e
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2023-11-22 09:25:37 +0100

    Test: JS direkt in HTML

--- signUp.js
+++ signUp.js
@@ -1,58 +1,42 @@
 function redirectLogin() {
     window.location.href = "https://benjiwurfl.github.io/Login/";
 }
-
-// Import the functions you need from the SDKs you need
+//function redirectApp() {
+//    window.location.href = "https://thinkwisenotes.webflow.io/app";
+//}<script type="module">
 import { initializeApp } from "firebase/app";
-
 import { getAnalytics } from "firebase/analytics";
-
-import { getFirestore } from "firebase/firestore";
-
+import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
     authDomain: "thinkwise-c7673.firebaseapp.com",
+    databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "thinkwise-c7673",
     storageBucket: "thinkwise-c7673.appspot.com",
     messagingSenderId: "37732571551",
     appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
     measurementId: "G-8957WM4SB7"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 const auth = getAuth(app);
 
-const db = getFirestore(app);
 
-document.getElementById('signupbutton').addEventListener('click', (e) => {
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-    var email = document.getElementById('inputEmail').value;
-    var password = document.getElementById('inputPassword').value;
-    var name = document.getElementById('inputName').value;
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            alert('user created');
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-
-            alert(errorMessage);
-            // ..
-        });
-
-
-})
+<
+/script>
